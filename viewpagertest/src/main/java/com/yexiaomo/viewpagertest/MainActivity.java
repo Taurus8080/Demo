164818afb1979f474eafc1f0e60ea017a7e7e8d3
Taurus <150025975@qq.com>
2018-05-30 09:39:10 +0800

    vp

--- viewpagertest/src/main/java/com/yexiaomo/viewpagertest/MainActivity.java
+++ viewpagertest/src/main/java/com/yexiaomo/viewpagertest/MainActivity.java
@@ -1,13 +1,46 @@
 package com.yexiaomo.viewpagertest;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.yexiaomo.viewpagertest.adapter.MyPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private TextView mTvText;
+    private String[] texts = {"蒲公英","海星","开始使用"};
+    private int[] imgsId = {R.drawable.a1, R.drawable.a2, R.drawable.a3};
+    private List<ImageView> imgList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
+
+        mViewPager.setAdapter(new MyPagerAdapter(imgList));
+    }
+
+    private void initData() {
+        for (int i=0; i<imgsId.length; i++){
+            ImageView iv = new ImageView(this);
+            iv.setImageResource(imgsId[i]);
+            imgList.add(iv);
+        }
+
+    }
+
+    private void initView() {
+        mViewPager = findViewById(R.id.view_pager);
+        mTvText = findViewById(R.id.tv_text);
+        mTvText.setText(texts[0]);
     }
 }
---
+++ viewpagertest/src/main/java/com/yexiaomo/viewpagertest/adapter/MyPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.yexiaomo.viewpagertest.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+public class MyPagerAdapter extends PagerAdapter {
+
+    private List<ImageView> imgList;
+
+    public MyPagerAdapter(List<ImageView> imgList) {
+        this.imgList = imgList;
+    }
+
+    @Override
+    public int getCount() {
+        return imgList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        return super.instantiateItem(container, position);
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        super.destroyItem(container, position, object);
+    }
+}
