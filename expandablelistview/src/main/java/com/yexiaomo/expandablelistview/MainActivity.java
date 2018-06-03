cd01418c02ddb5caac179f2173544c0457aa5049
Taurus <150025975@qq.com>
2018-06-03 19:34:35 +0800

    extv

--- expandablelistview/src/main/java/com/yexiaomo/expandablelistview/MainActivity.java
+++ expandablelistview/src/main/java/com/yexiaomo/expandablelistview/MainActivity.java
@@ -9,13 +9,14 @@ import com.yexiaomo.expandablelistview.adapter.MyBaseExpandableListAdapter;
 public class MainActivity extends AppCompatActivity {
 
     private ExpandableListView mExpendList;
-    private String[] groupStrings = {"西游记", "水浒传", "三国演义", "红楼梦"};
+    private String[] groupStrings = {"西游记","水浒传","三国演义","红楼梦"};
     private String[][] childStrings = {
             {"唐三藏", "孙悟空", "猪八戒", "沙和尚"},
             {"宋江", "林冲", "李逵", "鲁智深"},
             {"曹操", "刘备", "孙权", "诸葛亮", "周瑜"},
             {"贾宝玉", "林黛玉", "薛宝钗", "王熙凤"}
     };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
